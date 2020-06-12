3cfbc6ba2500bef03586a505aee7c89968c5bbd4
caliban <yuhecool@yahoo.com>
2020-06-12 12:08:09 +0800

    建库部分

--- 软件工程大作业/SqlServerPart/ChangeGoods.sql
+++ 软件工程大作业/SqlServerPart/ChangeGoods.sql
@@ -1,19 +1,20 @@
-create database Change
-on
-(
-name = 'Change',
-filename = 'E:\DATA\Change_data.mdf',
-size = 5,
-maxsize = 500,
-filegrowth = 10%
-)
+-- create database Change
+-- on
+-- (
+-- name = 'Change',
+-- filename = 'E:\DATA\Change_data.mdf',
+-- size = 5,
+-- maxsize = 500,
+-- filegrowth = 10%
+-- )
 
-log on
-(
-name = 'Change_log',
-filename = 'E:\DATA\Change_log.ldf',
-size = 3,
-filegrowth = 1
-)
+-- log on
+-- (
+-- name = 'Change_log',
+-- filename = 'E:\DATA\Change_log.ldf',
+-- size = 3,
+-- filegrowth = 1
+-- )
 
---建库部分
+--建库部分，自行根据数据库设置建库
+--默认用户名：admin 密码：123456
