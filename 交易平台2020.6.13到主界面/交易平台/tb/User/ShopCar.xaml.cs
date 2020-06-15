a19e1c1ab5115e6533aa94901303c45bd9673326
tangbi123 <50258846+tangbi123@users.noreply.github.com>
2020-06-15 14:24:32 +0800

    good 补充

--- 交易平台2020.6.13到主界面/交易平台/tb/User/ShopCar.xaml.cs
+++ 交易平台2020.6.13到主界面/交易平台/tb/User/ShopCar.xaml.cs
@@ -35,10 +35,15 @@ namespace 交易平台.tb.User
             DataSet ds = TbSql.GetDataSet(sqlstr);//DataSet 是查询结果
 
             //DataRow drs = ds.Tables[0].Rows[0];
-            
+
             //循环  将每一个商品记录 加入 listbox 里面
             for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
                 lb.Items.Add(new Frame().Content = new Good(ds.Tables[0].Rows[i]));
+                //Separator s = new Separator();
+                //s.Height = 1;
+                //lb.Items.Add(s);
+            }
         }
     }
 }
