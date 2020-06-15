352b4b8caef75cc90c98ef5eb53f6d5f585694d5
caliban <yuhecool@yahoo.com>
2020-06-15 17:05:24 +0800

    交易初步

--- 交易平台2020.6.13到主界面/交易平台/tb/User/Good.xaml.cs
+++ 交易平台2020.6.13到主界面/交易平台/tb/User/Good.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using 交易平台.yuhe;
+
 
 namespace 交易平台.tb.User
 {
@@ -42,6 +44,26 @@ namespace 交易平台.tb.User
             图片.Source = new BitmapImage(new Uri(dr[5].ToString(), UriKind.RelativeOrAbsolute));
             //new Uri(dr[5].ToString(), UriKind.Relative)
             商品信息.Text = "商品信息：" + dr[6].ToString();
+        }
+
+        private void Click_pay(object sender, RoutedEventArgs e)
+        {
+            //this.Content = new Frame();
+            //var pay = new consume();
+            //pay.ShowsNavigationUI;
+            //Content = new consume();
+            Window w = new Window();
+            w.Width = 800;
+            consume pay = new consume();
+            w.Content = pay;
+            pay.Detail_Image.Source = 图片.Source;
+            pay.ItemDetail_Textbox.Text = 商品信息.Text;
+            //pay.Type_TextBlock.Text = 
+            pay.ItemID_TextBlock.Text = 商品号.Text;
+            pay.ItemName_TextBox.Text = 商品名称.Text;
+            pay.price_TextBox.Text = 价格.Content.ToString();
+            w.Show();
+
         }
     }
 }
